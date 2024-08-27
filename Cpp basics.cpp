8d407424bc6ed147e83df2e220d1959a5d04b850
Dev Kumar <110048121+DevKumar57-67@users.noreply.github.com>
2024-08-27 23:00:43 +0530

    Add files via upload

---
+++ Cpp basics.cpp
@@ -0,0 +1,103 @@
+// In this tutorial we will learn the Cpp Basics
+//Cpp is a language derived from C with more features and high level language
+
+
+
+
+/*
+  First program in cpp
+  
+  
+#include <iostream>
+using namespace std;
+int main() {
+  cout <<"Hello Dev";
+  return 0;
+}
+
+  
+  
+// Variables in cpp
+
+
+#include <iostream>
+using namespace std;
+int main() {
+  
+  
+  int i,j,sum;
+  i = 56;
+  j = 78;
+  sum = i+j;
+  cout <<"The sum is " << sum << endl;
+  return 0;
+}
+
+
+  
+  
+//Taking input from user 
+
+#include <iostream>
+using namespace std;
+int main() {
+  int a,b,sum;
+ 
+  cout <<"Enter the first number";
+  
+  cin >> a; 
+  cout<<"Enter the second number";
+  cin >> b;
+  sum = a+b;
+  cout << "The sum of a and b is" << sum << endl;
+  return 0;
+}
+
+
+  
+  
+
+//swapping numbers 
+
+
+#include <iostream>
+using namespace std;
+int main() {
+  int n,m,temp;
+  cout << "Enter the first number";
+  cin >> n;
+  cout << "Enter the second number";
+  cin >> m;
+  temp = n;
+  n = m;
+  m = temp;
+  
+  cout <<"After swapping n =" << n << "m ="<<m << endl;
+  
+}
+
+
+
+  
+  
+// Loops in Cpp
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+  for (int i= 1;i<=10;i++)
+  {
+    cout <<"\nSquare of " <<i<< "is" << (i*i);
+    
+  }
+  return 0;
+}
+ 
+*/
+  
+  
+
+ 
